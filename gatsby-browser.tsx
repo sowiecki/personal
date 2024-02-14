a802c997c9be91383434e6d1b1f5cd86f87d147f
nase <nase00@gmail.com>
2024-02-14 11:38:33 -0600

    Workaround for theme bug.

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,13 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { navigate } from 'gatsby';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
-import Politik from './static/fonts/Politik.ttf';
-
-const primary = '#1A2027';
-const secondary = '#FB5012';
-const tertiary = '#01FDF6';
-const q = '#03FCBA';
 
 const theme = createTheme({
   palette: {
@@ -25,23 +19,6 @@ const theme = createTheme({
       contrastText: '#000',
     },
   },
-  // typography: {
-  //   fontFamily: 'Politik, Arial',
-  // },
-  // components: {
-  //   MuiCssBaseline: {
-  //     styleOverrides: `
-  //       @font-face {
-  //         font-family: 'Politik';
-  //         font-style: normal;
-  //         font-display: swap;
-  //         font-weight: 400;
-  //         src: local('Politik'), local('Politik-Regular'), url(${Politik}) format('ttf');
-  //         unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-  //       }
-  //     `,
-  //   },
-  // },
 });
 
 // Logs when the client route changes
@@ -49,9 +26,20 @@ const theme = createTheme({
 //   ...
 // }
 
-export const wrapPageElement = ({ element, props }) => (
-  <ThemeProvider theme={theme} {...props}>
+export const wrapPageElement = ({ element, props }) => {
+  const [hasRedirectedState, setHasRedirectedState] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !hasRedirectedState) {
+      setHasRedirectedState(true);
+      navigate('/redirect');
+    }
+  }, [hasRedirectedState]);
+
+  return (
+  <ThemeProvider theme={theme}>
     <CssBaseline />
     {element}
   </ThemeProvider>
 );
+  }
